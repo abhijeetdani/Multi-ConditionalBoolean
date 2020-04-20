323bf6c7adc8849e57190a1d2731e5aa534fe8c1
Abhijeet Dani <abhijeetdani@gmail.com>
2020-04-20 16:23:16 +0530

    adding the multi conditional boolean unity tutorial to the git

---
+++ Assets/MultiConditionalBoolean.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MultiConditionalBoolean<T> where T : Enum {
+
+	private int _value;
+	
+	public MultiConditionalBoolean() {
+		_value = 0;
+	}
+	
+	// Sets the bit
+	// Eg: If _value is 0, consider the first 4 rightmost bits 0000
+	// We left shit 1 by the int value of the enum, say its 0, so we get 0001
+	// Then we do a binary OR, and we get the following
+	// 0000
+	// 0001
+	// ----
+	// 0001 --> value after the OR operation, we have set the 1st bit
+	// All the other bits are unaffected
+	public void Set(T enumValue) {
+		try {
+			int enumIntValue = (int) Convert.ChangeType(enumValue, typeof(int));
+			_value = _value | (1 << enumIntValue);
+		} catch (InvalidCastException e) {
+			Debug.Log("Cant convert enum to int");
+		}
+	}
+	
+	// Resets the bit
+	// Eg: If _value is 3, consider the first 4 rightmost bits 0011
+	// We left shit 1 by the int value of the enum, say its 0, so we get 0001
+	// Then we get the complement of this which gives us, ~(0001) = 1110
+	// Then we do a binary AND, and we get the following
+	// 0011
+	// 1110
+	// ----
+	// 0010 --> _value after the AND operation, we have reset the 1st bit
+	// All other bits are unaffected
+	public void Reset(T enumValue) {
+		try {
+			int enumIntValue = (int) Convert.ChangeType(enumValue, typeof(int));
+			_value = _value & ~(1 << enumIntValue);
+		} catch(InvalidCastException e) {
+			Debug.Log("Cant convert enum to int");
+		}
+	}
+	
+	// If value is 0, then its false
+	// else true
+	public bool IsTrue() {
+		return _value != 0;
+	}
+	
+	public void Clear() {
+		_value = 0;
+	}
+}
---
+++ Assets/UIScreen.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum PlayerProtected {
+	Shield = 0,
+	Revive = 1,
+}
+
+
+public class UIScreen : MonoBehaviour {
+
+	[SerializeField] private float shieldLifetime = 10f;
+	[SerializeField] private float reviveLifetime = 5f;
+	[SerializeField] private Slider shieldSlider;
+	[SerializeField] private Slider reviveSlider;
+	[SerializeField] private Text protectedValue;
+	[SerializeField] private Button shieldButton;
+	[SerializeField] private Button reviveButton;
+
+	private MultiConditionalBoolean<PlayerProtected> _playerProtected;
+	
+	public bool IsPlayerProtected {
+		get {
+			return _playerProtected.IsTrue();
+		}
+	}
+
+	public void OnShieldButtonPressed() {
+		shieldButton.interactable = false;
+		// Set Player Protected
+		_playerProtected.Set(PlayerProtected.Shield);
+		protectedValue.text = IsPlayerProtected.ToString();
+		StartCoroutine(ShieldTimerCountdown());
+	}
+	
+	public void OnReviveButtonPressed() {
+		reviveButton.interactable = false;
+		// Set Player Protected
+		_playerProtected.Set(PlayerProtected.Revive);
+		protectedValue.text = IsPlayerProtected.ToString();
+		StartCoroutine(ReviveTimerCountdown());
+	}
+
+	private void Awake() {
+		_playerProtected = new MultiConditionalBoolean<PlayerProtected>();
+		protectedValue.text = IsPlayerProtected.ToString();
+	}
+
+	private IEnumerator ShieldTimerCountdown() {
+		float timer = shieldLifetime;
+		while (timer > 0f) {
+			shieldSlider.value = timer / shieldLifetime;
+			yield return null;
+			timer -= Time.deltaTime;
+		}
+
+		shieldSlider.value = 0f;
+		// Reset Player Protected
+		_playerProtected.Reset(PlayerProtected.Shield);
+		protectedValue.text = IsPlayerProtected.ToString();
+		shieldButton.interactable = true;
+	}
+	
+	private IEnumerator ReviveTimerCountdown() {
+		float timer = reviveLifetime;
+		while (timer > 0f) {
+			reviveSlider.value = timer / reviveLifetime;
+			yield return null;
+			timer -= Time.deltaTime;
+		}
+
+		reviveSlider.value = 0f;
+		// Reset Player Protected
+		_playerProtected.Reset(PlayerProtected.Revive);
+		protectedValue.text = IsPlayerProtected.ToString();
+		reviveButton.interactable = true;
+	}
+}
